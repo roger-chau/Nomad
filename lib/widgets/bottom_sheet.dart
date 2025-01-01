2ee190c1e040bbd2c6cfd10c0c6bd5dae707f82c
Roger Chau <54104821+roger-chau@users.noreply.github.com>
2025-01-01 11:57:44 -0700

    more refactor

--- lib/widgets/bottom_sheet.dart
+++ lib/widgets/bottom_sheet.dart
@@ -3,8 +3,8 @@ import 'package:provider/provider.dart';
 import 'package:budgeting_app/view_models/budget_view_model.dart';
 
 const List<Widget> creationButtons = [
-  Text("Budget"),
-  Text("Category"),
+  Text("Category", style: TextStyle(fontWeight: FontWeight.bold)),
+  Text("Budget", style: TextStyle(fontWeight: FontWeight.bold)),
 ];
 
 class BottomSheetWidget extends StatefulWidget {
@@ -18,6 +18,28 @@ class CreateBottomSheetState extends State<BottomSheetWidget> {
   final List<bool> _selectedCreation = [true, false];
   final TextEditingController controller = TextEditingController();
 
+  Widget _buildCategoryView() {
+    return Center(
+        child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              controller: controller,
+              decoration: const InputDecoration(
+                  labelText: "Category Name", border: OutlineInputBorder()),
+            )));
+  }
+
+  Widget _buildBudgetView() {
+    return Center(
+        child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              controller: controller,
+              decoration: const InputDecoration(
+                  labelText: "Budget Name", border: OutlineInputBorder()),
+            )));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -25,7 +47,7 @@ class CreateBottomSheetState extends State<BottomSheetWidget> {
       color: Theme.of(context).colorScheme.surfaceContainer,
       child: Stack(children: <Widget>[
         Positioned(
-            top: 10,
+            top: 20,
             left: 0,
             right: 0,
             child: Center(
@@ -38,25 +60,14 @@ class CreateBottomSheetState extends State<BottomSheetWidget> {
                   }
                 });
               },
+              borderRadius: BorderRadius.circular(10),
               selectedColor: Colors.white,
               fillColor: Colors.blue,
               color: Colors.grey,
+              constraints: BoxConstraints.expand(width: 150),
               children: creationButtons,
             ))),
-        Center(
-            child: Padding(
-                padding: EdgeInsets.all(16),
-                child: Row(children: [
-                  Icon(Icons.add),
-                  Expanded(
-                      child: Padding(
-                          padding: EdgeInsets.only(left: 8),
-                          child: TextField(
-                              controller: controller,
-                              decoration: InputDecoration(
-                                  hintText: "Enter Budget Name",
-                                  border: OutlineInputBorder()))))
-                ]))),
+        _selectedCreation[0] ? _buildCategoryView() : _buildBudgetView(),
         Positioned(
             bottom: 50,
             right: 25,
@@ -66,7 +77,7 @@ class CreateBottomSheetState extends State<BottomSheetWidget> {
                     budget.addCategory(controller.text);
                     Navigator.pop(context);
                   },
-                  child: const Text("Add Budget"));
+                  child: const Text("Add"));
             }))
       ]),
     );
